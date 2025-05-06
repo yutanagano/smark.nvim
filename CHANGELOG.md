6bc2b05ac948694ecd703c843bcaa4e27ef8a073
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-05-06 21:45:00 +0100

    feat: implement bullet insertion with o from normal mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 ## Version 0.0.1
 
 - [x] Automatically insert bullets with <CR>
-- [ ] Handle insertion with "o"
+- [x] Handle insertion with "o"
 - [ ] Handle multiple bullet types (-, \*, +)
 - [ ] Handle checkbox bullets
 - [ ] Handle numbered bullets
--- lua/smark.lua
+++ lua/smark.lua
@@ -5,21 +5,42 @@ Module.setup = function(_)
 	-- nothing for now
 end
 
-Module.carriage_return = function()
-	local cursor_line_num = vim.fn.line(".")
-	local next_line_num = cursor_line_num + 1
-	local cursor_line_text = vim.fn.getline(".")
-	local cursor_on_bulleted_line = utils.is_bulleted_line(cursor_line_text)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "text" },
+	callback = function()
+		vim.keymap.set("i", "<CR>", function()
+			local cursor_row_1_based, _ = table.unpack(vim.api.nvim_win_get_cursor(0))
+			local cursor_line_text = vim.api.nvim_buf_get_lines(0, cursor_row_1_based - 1, cursor_row_1_based, true)[1]
+			local cursor_on_bulleted_line = utils.is_bulleted_line(cursor_line_text)
 
-	if cursor_on_bulleted_line then
-		vim.fn.append(cursor_line_num, "- ")
-		local col = string.len(vim.fn.getline(next_line_num)) + 1
-		vim.fn.setpos(".", { 0, next_line_num, col })
-	else
-		local cr = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
-		vim.api.nvim_feedkeys(cr, "n", false)
-	end
-end
+			if cursor_on_bulleted_line then
+				vim.api.nvim_buf_set_lines(0, cursor_row_1_based, cursor_row_1_based, true, { "- " })
+				local target_col_1_based = string.len(
+					vim.api.nvim_buf_get_lines(0, cursor_row_1_based, cursor_row_1_based + 1, true)[1]
+				) + 1
+				vim.api.nvim_win_set_cursor(0, { cursor_row_1_based + 1, target_col_1_based })
+			else
+				local cr = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+				vim.api.nvim_feedkeys(cr, "n", false)
+			end
+		end, { buffer = true })
+		vim.keymap.set("n", "o", function()
+			local cursor_row_1_based, _ = table.unpack(vim.api.nvim_win_get_cursor(0))
+			local cursor_line_text = vim.api.nvim_buf_get_lines(0, cursor_row_1_based - 1, cursor_row_1_based, true)[1]
+			local cursor_on_bulleted_line = utils.is_bulleted_line(cursor_line_text)
+
+			if cursor_on_bulleted_line then
+				vim.api.nvim_buf_set_lines(0, cursor_row_1_based, cursor_row_1_based, true, { "- " })
+				vim.api.nvim_win_set_cursor(0, { cursor_row_1_based + 1, 0 })
+				vim.cmd("startinsert!")
+			else
+				vim.api.nvim_buf_set_lines(0, cursor_row_1_based, cursor_row_1_based, true, { "" })
+				vim.api.nvim_win_set_cursor(0, { cursor_row_1_based + 1, 0 })
+				vim.cmd("startinsert!")
+			end
+		end, { buffer = true })
+	end,
+})
 
 --- @param line_text string Text of line to evaluate
 --- @return boolean is_bulleted True if line_num is a bulleted line, false otherwise
@@ -31,12 +52,5 @@ utils.is_bulleted_line = function(line_text)
 	end
 	return false
 end
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "markdown", "text" },
-	callback = function()
-		vim.keymap.set("i", "<CR>", Module.carriage_return, { buffer = true })
-	end,
-})
 
 return Module
