fd2859baf56eed01a36c7c874a3ae5430335216d
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-07-04 21:47:05 +0100

    chore: cleaning up custom type definitions

--- lua/smark/types.lua
+++ lua/smark/types.lua
@@ -1,23 +1,19 @@
+---@class ListItem
+---@field spec ListSpec List item characteristics necessary to properly render it.
+---@field content string[] The text content of the list item. Each string in the array will be rendered on a new line.
+
+---@class ListSpec
+---@field is_ordered boolean
+---@field is_task boolean
+---@field is_completed boolean True if task which is marked completed.
+---@field index integer The item index number.
+---@field indent_spaces integer The indentation level of the line in number of spaces. -1 results in a line with no list marker element.
+
 ---@alias indent_spec IndentLevelSpec[] Integer array describing the number of indent spaces required to align to each level, up to current
 
 ---@class IndentLevelSpec
 ---@field is_ordered boolean
 ---@field indent_spaces integer
-
----@class ListItem
----@field spec ListSpec List item characteristics necessary to properly render it.
----@field content string[] The text content of the list item.
-
----@class ListSpec
----@field is_ordered boolean True if ordered list element.
----@field is_task boolean True if task list element.
----@field is_completed boolean True if task which is marked completed.
----@field index integer The item index number.
----@field indent_spaces integer The indentation level of the line in number of spaces. -1 results in a line with no list marker element.
-
----@class TextBlockBounds
----@field upper integer 1-indexed upper bound line number
----@field lower integer 1-indexed lower bound line number
 
 ---@class CursorCoords
 ---@field row1 integer 1-indexed row number of cursor
@@ -25,5 +21,9 @@
 
 ---@class LiCursorCoords
 ---@field list_index integer index of list item that the cursor is inside
----@field content_lnum integer the line number relative to the list item contents that the cursor is on
+---@field content_lnum integer 1-indexed line number relative to the list item contents that the cursor is on
 ---@field col 0-indexed column number of cursor
+
+---@class TextBlockBounds
+---@field upper integer 1-indexed upper bound line number
+---@field lower integer 1-indexed lower bound line number
